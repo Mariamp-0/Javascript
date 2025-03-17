a269fe5648eb56ec8fff44d7cb326e6d5fc014bc
Maria Paula Mejía <mapis4045@gmail.com>
2025-03-17 09:45:17 -0500

    Información general del lenguaje javascript

---
+++ script.js
@@ -0,0 +1,128 @@
+//console.log('Hola mundo')
+
+//prompt es usado para pedir información desde el navegador.
+
+//let nombre = prompt('¿Cuál es tu nombre?')
+//console.log('Hola '+ nombre + '!')
+
+/* 
+
+Las variables se declaran de tres posibles maneras. Let, const y var. Las variables son un espacio
+reservado para alguna información. Esta información se puede usar más adelante en el código
+
+let ---- se le puede reasignar su valor.
+const --- no es posible cambiarla, es constante.
+
+
+
+let numero = 100
+numero = 200
+//console.log(numero)
+
+//Para ver qué tipo de dato es se usa el typeof con el nombre de la variable. 
+
+//console.log(typeof nombre)
+
+//Tipos de variables
+
+let number = 10; // Number
+let texto = "Hola de nuevo" // String
+let verdadero = true; //Boolean
+let falso = false; // Boolean
+let nodefinido; // Undefined
+let indefinido; // Undefined
+let nulo = null //Null
+
+/*
+console.log(typeof number);
+console.log(typeof texto);
+console.log(typeof verdadero);
+console.log(typeof falso);
+console.log(typeof nodefinido);
+console.log(typeof indefinido);
+console.log(typeof nulo);
+
+*/
+
+// Tipos de operadores
+
+//Aritméticos 
+
+/*
+
+let a = 20;
+let b = 5;
+
+let multiplicacion = a * b;
+let division = a / b
+let exponente = a ** b
+let modulo = a % b
+
+//Suma
+
+console.log(a + b); //25
+
+//Resta 
+
+console.log( a - b) //15
+
+// Multiplicación
+
+console.log(multiplicacion) //100
+
+//División
+
+console.log(division) // 4
+
+//Exponente
+
+console.log(exponente) // 3200000
+
+//Módulo
+
+console.log(modulo) // 0
+
+
+// ++ = le suma 1   -- = le resta 1
+let contador = 1;
+contador++;
+
+console.log(contador)
+
+
+*/
+//Asignación
+
+/*
+
+let a = 5;
+a += 10; // a = a + 10
+
+a += 
+a -= 
+a *= 
+a /= 
+
+
+console.log(a);
+
+*/
+//Comparación -- Devuelven booleanos
+
+// == igual a 
+// === igual valor y tipo
+// != no igual
+
+
+
+// Condicional if y else if
+
+let number = prompt("Ingrese un número")
+
+if (number >= 0){
+        alert("El número es positivo");
+} else{
+    alert("El número es negativo")
+}
+
+console.log("Siguiente flujo")
