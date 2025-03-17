ccc6cade8879348b9459576abc8797cf6edbd59f
Maria Paula Mejía <mapis4045@gmail.com>
2025-03-17 10:26:02 -0500

    Agrega la solución del primer punto del taller, usando condicionales if, else , else if

---
+++ taller.js
@@ -0,0 +1,38 @@
+
+//Taller en clase
+
+
+// 1.
+let pais = prompt("Ingrese el país")
+let idioma = ""
+
+if (pais === "Colombia"){
+    idioma = "Español"
+} else if (pais === "Estados Unidos"){
+    idioma = "Inglés"
+} else if(pais === "Inglaterra"){
+    idioma = "Inglés"
+} else if (pais === "Canada"){
+    idioma = "Inglés y Francés"
+} else if (pais === "Francia"){
+    idioma = "Francés"
+} else if (pais === "Italia"){
+    idioma = "Italiano"
+} else if (pais === "Alemania"){
+    idioma = "Alemám"
+} else if (pais === "China"){
+    idioma = "Mandarín"
+} else if (pais === "Japon"){
+    idioma = "Japonés"
+} else if (pais === "Brasil"){
+    idioma = "Portugués"
+} else {
+    console.log("No existe el pais")
+}
+
+
+console.log(pais)
+console.log(idioma)
+
+//2.
+
